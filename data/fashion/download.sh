13b3ef837e141830df85b6f431e10463a78dbc82
Hyung-Kwon Ko <hkko@hcil.snu.ac.kr>
2020-02-09 17:16:16 +0900

    shell script for downloading datasets

---
+++ data/fashion/download.sh
@@ -0,0 +1,13 @@
+remove=$1
+if [ "$remove" = true ]; then
+  echo "Removing Fashion MNIST Dataset..."
+  rm *.gz
+  echo "Removed!"
+else
+  echo "Downloading Fashion MNIST Dataset..."
+  wget "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz" -O train-images-idx3-ubyte.gz
+  wget "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz" -O train-labels-idx1-ubyte.gz
+  wget "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz" -O t10k-images-idx3-ubyte.gz
+  wget "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz" -O t10k-labels-idx1-ubyte.gz
+  echo "Download Finished!"
+fi
